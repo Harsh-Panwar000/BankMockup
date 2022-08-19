409c960375f38f1de2b9c73a88e8d2f8f0beca0a
Harshit Panwawr <63825340+Harsh-Panwar000@users.noreply.github.com>
2022-08-18 22:21:38 -0400

    Update index.js

--- website/static/index.js
+++ website/static/index.js
@@ -1,12 +1,3 @@
-function deleteNote(noteId) {
-  fetch("/delete-note", {
-    method: "POST",
-    body: JSON.stringify({ noteId: noteId }),
-  }).then((_res) => {
-    window.location.href = "/";
-  });
-}
-
 function toggle_display(){
   var ele = document.getElementsByClassName('.addAccountButton'); 
   if (ele.value == "Add new checking account"){
